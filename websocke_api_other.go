c17cefda5be247a6be7d3d418e5ee62518e08480
chaster <ntfdance@gmail.com>
2024-07-13 22:18:27 +0800

    account balance

--- websocke_api_other.go
+++ websocke_api_other.go
@@ -10,6 +10,10 @@ func (w *WebsocketAPIClient) NewPositionInfoService() *PositionInfoService {
 	return &PositionInfoService{websocketAPI: w}
 }
 
+func (w *WebsocketAPIClient) NewFuturesAccountBalanceService() *FuturesAccountBalanceService {
+	return &FuturesAccountBalanceService{websocketAPI: w}
+}
+
 type PositionInfoService struct {
 	websocketAPI *WebsocketAPIClient
 	recvWindow   *int64
@@ -88,4 +92,77 @@ type PositionInformation struct {
 	UnRealizedProfit string `json:"unRealizedProfit"`
 	PositionSide     string `json:"positionSide"`
 	UpdateTime       int    `json:"updateTime"`
+}
+
+type FuturesAccountBalanceService struct {
+	websocketAPI *WebsocketAPIClient
+	recvWindow   *int64
+}
+
+func (s *FuturesAccountBalanceService) RecvWindow(recvWindow int64) *FuturesAccountBalanceService {
+	s.recvWindow = &recvWindow
+	return s
+}
+
+type FuturesAccountBalanceResponse struct {
+	ID         string              `json:"id"`
+	Status     int                 `json:"status"`
+	Error      *WsAPIErrorResponse `json:"error,omitempty"`
+	Result     []*AssetBalance     `json:"result,omitempty"`
+	RateLimits []*WsAPIRateLimit   `json:"rateLimits"`
+}
+
+type AssetBalance struct {
+	AccountAlias       string `json:"accountAlias"`
+	Asset              string `json:"asset"`
+	Balance            string `json:"balance"`
+	CrossWalletBalance string `json:"crossWalletBalance"`
+	CrossUnPnl         string `json:"crossUnPnl"`
+	AvailableBalance   string `json:"availableBalance"`
+	MaxWithdrawAmount  string `json:"maxWithdrawAmount"`
+	MarginAvailable    bool   `json:"marginAvailable"`
+	UpdateTime         int64  `json:"updateTime"`
+}
+
+func (s *FuturesAccountBalanceService) Do(ctx context.Context) (*FuturesAccountBalanceResponse, error) {
+	parameters := map[string]string{}
+
+	if s.recvWindow != nil {
+		parameters["recvWindow"] = strconv.FormatInt(*s.recvWindow, 10)
+	}
+
+	signedParams, err := websocketAPISignature(s.websocketAPI.APIKey, s.websocketAPI.APISecret, parameters)
+	if err != nil {
+		panic(err)
+	}
+
+	id := getUUID()
+
+	payload := map[string]interface{}{
+		"id":     id,
+		"method": "account.balance",
+		"params": signedParams,
+	}
+
+	messageCh := make(chan []byte)
+	s.websocketAPI.ReqResponseMap[id] = messageCh
+
+	err2 := s.websocketAPI.SendMessage(payload)
+	if err2 != nil {
+		return nil, err2
+	}
+
+	defer delete(s.websocketAPI.ReqResponseMap, id)
+
+	select {
+	case response := <-messageCh:
+		var accInfoResponse FuturesAccountBalanceResponse
+		err = json.Unmarshal(response, &accInfoResponse)
+		if err != nil {
+			return nil, err
+		}
+		return &accInfoResponse, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
 }
