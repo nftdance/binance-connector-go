3c53f34424d2282c1d9a20f2f068e46651aae2d0
chaster <ntfdance@gmail.com>
2024-07-15 17:58:28 +0800

    position risk

--- futures_account.go
+++ futures_account.go
@@ -168,6 +168,10 @@ func (c *Client) NewFuturesGetBalanceService() *FuturesGetBalanceService {
 	return &FuturesGetBalanceService{c: c}
 }
 
+func (c *Client) GetPositionRiskService() *GetPositionRiskService {
+	return &GetPositionRiskService{c: c}
+}
+
 type FuturesCreateOrderService struct {
 	c                *Client
 	symbol           string
@@ -496,4 +500,57 @@ func (s *FuturesGetBalanceService) Do(ctx context.Context, opts ...RequestOption
 		return []*Balance{}, err
 	}
 	return res, nil
+}
+
+// GetPositionRiskService get account balance
+type GetPositionRiskService struct {
+	c      *Client
+	symbol string
+}
+
+// Symbol set symbol
+func (s *GetPositionRiskService) Symbol(symbol string) *GetPositionRiskService {
+	s.symbol = symbol
+	return s
+}
+
+// Do send request
+func (s *GetPositionRiskService) Do(ctx context.Context, opts ...RequestOption) (res []*PositionRisk, err error) {
+	r := &request{
+		method:   http.MethodGet,
+		endpoint: "/fapi/v2/positionRisk",
+		secType:  secTypeSigned,
+	}
+	if s.symbol != "" {
+		r.setParam("symbol", s.symbol)
+	}
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return []*PositionRisk{}, err
+	}
+	res = make([]*PositionRisk, 0)
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return []*PositionRisk{}, err
+	}
+	return res, nil
+}
+
+// PositionRisk define position risk info
+type PositionRisk struct {
+	EntryPrice       string `json:"entryPrice"`
+	BreakEvenPrice   string `json:"breakEvenPrice"`
+	MarginType       string `json:"marginType"`
+	IsAutoAddMargin  string `json:"isAutoAddMargin"`
+	IsolatedMargin   string `json:"isolatedMargin"`
+	Leverage         string `json:"leverage"`
+	LiquidationPrice string `json:"liquidationPrice"`
+	MarkPrice        string `json:"markPrice"`
+	MaxNotionalValue string `json:"maxNotionalValue"`
+	PositionAmt      string `json:"positionAmt"`
+	Symbol           string `json:"symbol"`
+	UnRealizedProfit string `json:"unRealizedProfit"`
+	PositionSide     string `json:"positionSide"`
+	Notional         string `json:"notional"`
+	IsolatedWallet   string `json:"isolatedWallet"`
 }
